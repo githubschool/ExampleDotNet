4f7356ee081a908b2ca84abe3e339dd45f53b991
Göran Öberg <goran.oberg@gmail.com>
2016-04-19 12:41:59 +0200

    Added Nationality

--- Models/GoranObergUser.cs
+++ Models/GoranObergUser.cs
@@ -9,5 +9,6 @@ namespace MvcSample.Web.Models
         public string Name { get; set; }
         public string Address { get; set; }
         public int Age { get; set; }
+        public string Nationality { get; set; }
     }
 }
