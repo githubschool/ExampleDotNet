f9cc381a12627be11942fa4894ed9e63fbb73982
Granroth Dan <SSSDGT@global.scd.scania.com>
2016-04-19 13:49:50 +0200

    ny property

--- Models/dgfreememUser.cs
+++ Models/dgfreememUser.cs
@@ -2,12 +2,13 @@
 
 namespace MvcSample.Web.Models
 {
-    public class User
+    public class DgfreememUser
     {
         [Required]
         [MinLength(4)]
         public string Name { get; set; }
         public string Address { get; set; }
         public int Age { get; set; }
+        public double Weight { get; set; }
     }
 }
