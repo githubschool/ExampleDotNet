8316a3804abb86550d7998d574ab646f8d746637
cannqa <CANNQA@global.scd.scania.com>
2016-04-19 15:50:04 +0200

    test fixed

--- Tests/CannqaTest.cs
+++ Tests/CannqaTest.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Xunit;
+using MvcSample.Web.Models;
 
 /// <summary>
 /// Summary description for CannqaTest
 /// </summary>
-public class CannqaTest
+namespace MvcSample.Tests
 {
-	public CannqaTest()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+    public class CannqaTest
+    {
+        [Fact]
+        public CannqaTest()
+        {
+            User user = new User()
+            {
+                Name = "Catrin Andersson"
+            };
+            Assert.Equal("Catrin Andersson", user.Name);
+        }
+        
+    }
 }
