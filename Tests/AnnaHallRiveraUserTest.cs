1c1cde4d27ddd8f000263623a5cb358ae3b69dd8
AnnaHallRivera <AHAG0G@global.scd.scania.com>
2016-04-19 11:38:58 +0200

    added AnnaHallRivera model and test

---
+++ Tests/AnnaHallRiveraUserTest.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using MvcSample.Web.Models;
+
+namespace MvcSample.Tests
+{
+    public class UserTest
+    {
+        [Fact]
+        public void NameTest()
+        {
+            User user = new User()
+            {
+                Name = "Anna Häll Rivera"
+            };
+            Assert.Equal("Anna Häll Rivera", user.Name);
+        }
+    }
+}
