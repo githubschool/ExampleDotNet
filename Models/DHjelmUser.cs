4808f749f77578388df42e55fe27dddac44a54fa
dhjczf <DHJCZF@global.scd.scania.com>
2016-04-19 11:38:25 +0200

    adding the new model and test DHjelmUser

---
+++ Models/DHjelmUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DHjelmUser
+/// </summary>
+public class DHjelmUser
+{
+	public DHjelmUser()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
---
+++ Tests/DHjelmUserTest .cs
@@ -0,0 +1,18 @@
+using Xunit;
+using MvcSample.Web.Models;
+
+namespace MvcSample.Tests
+{
+    public class DHjelmUserTest
+    {
+        [Fact]
+        public void NameTest()
+        {
+          DHjelmUser user = new DHjelmUser()
+            {
+                Name = "Daniel Hjelm"
+            };
+            Assert.Equal("Daniel Hjelm", user.Name);
+        }
+    }
+}
