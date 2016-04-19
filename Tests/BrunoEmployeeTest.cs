61fa6bf4c97ba49bf11f0e43cea3f93211d95ec1
Bruno Corte <brunocorte@msn.com>
2016-04-19 13:21:10 +0200

    Changing tests methods in order to pass

--- Tests/BrunoEmployeeTest.cs
+++ Tests/BrunoEmployeeTest.cs
@@ -7,7 +7,7 @@ namespace MvcSample.Tests
    {
        [Theory]
        [InlineData("Bruno",65000,26, "IKB")]
-       public void SalaryCheckTest(string name, long salary, int age, string department)
+       public void RichUserTest(string name, long salary, int age, string department)
        {
            var employee = new BrunoEmployee() {
                Name = name,
@@ -16,7 +16,22 @@ namespace MvcSample.Tests
                Department = department
            };
            
-           Assert.False(employee.Salary > 60000, "You're too rich! Get out!");
+           Assert.True(employee.Salary > 60000, "Not rich enough! Get out!");
        }
+       
+       [Theory]
+       [InlineData("John",5000,52, "IS")]
+       public void PoorUserTest(string name, long salary, int age, string department)
+       {
+           var employee = new BrunoEmployee() {
+               Name = name,
+               Salary = salary,
+               Age = age,
+               Department = department
+           };
+           
+           Assert.True(employee.Salary < 10000, "Not poor enough, pal!");
+       }
+       
    }
 }
