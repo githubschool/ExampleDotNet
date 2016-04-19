2597f665a5cf2a68c2d85b5a991c36b8c920f89a
Kjellén Lennart <lennart.kjellen@scania.com>
2016-04-19 13:13:58 +0200

    Corrected test

--- Tests/sssnnt_UserTest.cs
+++ Tests/sssnnt_UserTest.cs
@@ -8,7 +8,7 @@ namespace MvcSample.Tests
         [Fact]
         public void NameTest()
         {
-            User user = new User()
+            User user = new sssnnt_User()
             {
                 Name = "Lennart Kjellén"
             };
