e89fd73f5436f97e7c6869c39d75b9ee074e2d16
cannqa <CANNQA@global.scd.scania.com>
2016-04-19 11:38:34 +0200

    add cannqa test

---
+++ Tests/CannqaTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for CannqaTest
+/// </summary>
+public class CannqaTest
+{
+	public CannqaTest()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
