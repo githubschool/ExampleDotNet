b73ca5c7e22de34a6987b71d4c6e18267558cdad
woosh <SSSOWO@global.scd.scania.com>
2016-04-19 14:12:07 +0200

    Extended WooshUser to include properties for Slack, Twitter, Skype.

--- Models/WooshUser.cs
+++ Models/WooshUser.cs
@@ -5,5 +5,8 @@ namespace MvcSample.Web.Models
     public class WooshUser : User
     {
         public string Email {get; set; }
+        public string Slack { get; set; }
+        public string Skype { get; set; }
+        public string Twitter { get; set; }
     }
 }
--- Tests/WooshUserTest.cs
+++ Tests/WooshUserTest.cs
@@ -14,5 +14,36 @@ namespace MvcSample.Tests
             };
             Assert.Equal("mail@example.com", user.Email);
         }
+
+        [Fact]
+        public void SkypeTest()
+        {
+            var user = new WooshUser()
+            {
+                Skype = "SkypeUser"
+            };
+            Assert.Equal("SkypeUser", user.Skype);
+        }
+
+        [Fact]
+        public void SlackTest()
+        {
+            var user = new WooshUser()
+            {
+                Slack = "a-slack-handle"
+            };
+            Assert.Equal("a-slack-handle", user.Slack);
+        }
+
+        [Fact]
+        public void TwitterTest()
+        {
+            var user = new WooshUser()
+            {
+                Twitter = "justAnotherAccount"
+            };
+            Assert.Equal("justAnotherAccount", user.Twitter);
+        }
+
     }
 }
